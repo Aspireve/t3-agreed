961497113a5ffec893872fb682eae7319b74c3da
Aspireve <steve18fernandes@gmail.com>
2025-01-23 19:46:30 +0530

    chore: some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "@/trpc/react";
-import { ToastProvider } from "@/components/ui/toast";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
