10a75e4fd831250b118ce2ac30850ba920bbc672
Aspireve <steve18fernandes@gmail.com>
2025-02-10 19:52:15 +0530

    chore: added more auth

---
+++ server/src/auth/stratergy/local.stratergy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      usernameField: 'email',
+    });
+  }
+
+  async validate(username: string, password: string) {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
